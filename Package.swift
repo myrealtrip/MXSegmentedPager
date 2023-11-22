c5eaae377a1b2b83f8e65f6cce47ae5a43dcf4ff
백두산(iOS개발팀) <doosan.baek@myrealtrip.com>
2023-11-22 12:00:19 +0900

    test

--- Package.swift
+++ Package.swift
@@ -10,16 +10,16 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "https://github.com/maxep/MXPagerView.git", from: "0.2.0"),
-//        .package(url: "https://github.com/maxep/MXSegmentedControl.git", from: "1.1.0"),
-//        .package(url: "https://github.com/maxep/MXParallaxHeader.git", from: "1.0.0"),
+        .package(url: "https://github.com/myrealtrip/MXSegmentedControl.git", from: "1.1.0"),
+        .package(url: "https://github.com/myrealtrip/MXParallaxHeader.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "MXSegmentedPager",
             dependencies: [
 //                "MXPagerView",
-//                "MXSegmentedControl",
-//                "MXParallaxHeader"
+                "MXSegmentedControl",
+                "MXParallaxHeader"
             ]
         )
     ]
